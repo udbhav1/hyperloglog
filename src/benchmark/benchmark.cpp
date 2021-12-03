33af3b897fa5c66b369af083c4b5d55165923226
udbhav1 <umuthakana@gmail.com>
2021-12-03 17:43:43 -0500

    fix gitignore

---
+++ src/benchmark/benchmark.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <set>
+#include "../lib/memory.h"
+
+int main(){
+   // TODO fstream slow??
+   // path relative to final executable in project root
+   std::ifstream fin("./data/cleaned-shakespeare.txt");
+
+   std::set<std::string> s;
+   std::string word;
+   long long int tot = 0;
+   while(fin >> word){
+      s.insert(word);
+      tot++;
+   }
+
+   std::cout << "Total words:    " << tot << "\n";
+   std::cout << "Distinct words: " << s.size() << "\n\n";
+
+   getMemoryUsage();
+}
